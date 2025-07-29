351260a64901e6110db8eb15a065ec0d9ea65eb1
MR.MMD2019 <iran.mohammad1389@gmail.com>
2025-07-29 14:55:18 +0330

    Update content.js

--- content.js
+++ content.js
@@ -1,1 +1,32 @@
+import { solveImageCaptcha } from './solver/imageSolver.js';
 
+function findCaptchaImage() {
+  // معمول‌ترین انتخابگرهای کپچا
+  const selectors = ['img[alt*="captcha"]', 'img[src*="captcha"]', 'img[class*="captcha"]'];
+  for (const sel of selectors) {
+    const img = document.querySelector(sel);
+    if (img) return img;
+  }
+  return null;
+}
+
+function insertAnswer(inputElement, text) {
+  if (inputElement) {
+    inputElement.value = text;
+    inputElement.dispatchEvent(new Event('input', { bubbles: true }));
+  }
+}
+
+(async () => {
+  const image = findCaptchaImage();
+  if (!image) {
+    console.log('[Captcha Crusher] تصویری از کپچا پیدا نشد.');
+    return;
+  }
+
+  const result = await solveImageCaptcha(image);
+  console.log('[Captcha Crusher] نتیجه حل کپچا:', result);
+
+  const input = document.querySelector('input[type="text"], input[name*="captcha"]');
+  insertAnswer(input, result);
+})();
